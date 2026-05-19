f8950faf67c47e33f637e1b1af92a1a55dcab77b
Nikhil <nikhiltayde97@gmail.com>
2026-05-20 01:46:48 +0530

    bug: cors issue resolved, link updated

--- studybuddy-backend/app.js
+++ studybuddy-backend/app.js
@@ -1,63 +1,64 @@
-const express = require('express');
-const cors = require('cors')
-const helmet = require('helmet') //read about this
-const morgan = require('morgan') // RAT
-const rateLimit = require('express-rate-limit')
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet"); //read about this
+const morgan = require("morgan"); // RAT
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
 // ------------------------Security Middleware -------------------------
 
-app.use(helmet())
-app.use(cors())
+app.use(helmet());
+app.use(cors());
 
 // ------------------------Rate Limiter----------------------
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 min
-    max: 100,
-    message:'Too many requests, pleasae try after 15 minutes.'
-})
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 100,
+  message: "Too many requests, pleasae try after 15 minutes.",
+});
 
-app.use('/api', limiter)
+app.use("/api", limiter);
 
 // ------------------------Request Logger----------------------
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 // -------------------------Body Parser-----------------------
-app.use(express.json())
+app.use(express.json());
 
 // ------------------------health check route----------------
 
-app.get('/', (req,res)=> {
-    res.json({message:'📚 StudyBuddy API is running!'})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "📚 StudyBuddy API is running!" });
+});
 
 //--------------- Routes -------------------------------
-const authRoutes = require('./routes/authRoutes')
-app.use('/api/auth', authRoutes)
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 
-const pathRoutes = require('./routes/pathRoutes')
-app.use('/api/paths', pathRoutes)
+const pathRoutes = require("./routes/pathRoutes");
+app.use("/api/paths", pathRoutes);
 
-const chatRoutes = require('./routes/chatRoutes')
-app.use('/api/chat',chatRoutes)
+const chatRoutes = require("./routes/chatRoutes");
+app.use("/api/chat", chatRoutes);
 
-const quizRoutes = require('./routes/quizRoutes')
-app.use('/api/quiz', quizRoutes)
+const quizRoutes = require("./routes/quizRoutes");
+app.use("/api/quiz", quizRoutes);
 
-const adminRoutes = require('./routes/adminRoutes')
-app.use('/api/admin', adminRoutes)
+const adminRoutes = require("./routes/adminRoutes");
+app.use("/api/admin", adminRoutes);
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}))
+app.use(
+  cors({
+    origin: "https://study-buddy-nine-phi.vercel.app",
+    credentials: true,
+  }),
+);
 
 // ------------------------404 handler-------------------------
-app.use( (req, res) => {
-    res.status(404).json({message:'Route not found'})
-})
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
-module.exports = app
-
+module.exports = app;
